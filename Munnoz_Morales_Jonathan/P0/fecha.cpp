dd206987f479d00f32731fb719623bd0527f495d
Jonathan <jonathan@joni-HP-Pavilion-Gaming-Laptop-15-ec2xxx.home>
2023-03-28 18:20:30 +0200

    Añadido interfaz fecha e implementación de fecha

---
+++ Munnoz_Morales_Jonathan/P0/fecha.cpp
@@ -0,0 +1,145 @@
+#include "fecha.hpp"
+#include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <locale>
+
+Fecha::Fecha(int dia, int mes, int anno):dia_(dia), mes_(mes), anno_(anno)
+{
+	std::time_t tiempo_calendario = std::time(nullptr);
+	std::tm* tiempo_descompuesto = std::localtime(&tiempo_calendario);
+
+	if(anno_ == 0)
+		anno_ = tiempo_descompuesto->tm_year + 1900;
+
+	if(mes_ == 0)
+		mes_ = tiempo_descompuesto->tm_mon +1;
+
+	if(dia_ == 0)
+		dia_ = tiempo_descompuesto->tm_mday;
+
+	if(dia != 0 || mes != 0 || anno != 0)
+		esValida();
+}
+
+Fecha::Fecha(const char* fecha)
+{
+	int dia, mes, anno;
+	if(sscanf(fecha, "%d/%d/%d",&dia, %mes, %anno) != 3)
+	{
+		Fecha::Invalida format("Formato incorrecto por parte de la cadena recibida.");
+		throw format;
+	}
+
+	Fecha f(dia, mes, anno);
+	*this = f;
+}
+
+Fecha::operator const char*() const
+{
+	std::locale::global(std::locale("es_ES.utf8"));
+	char* aux = new char[100];
+
+	std::time_t tiempo_calendario = std::time(nullptr);
+	std::tm* f = std::localtime(&tiempo_calendario);
+
+	f->tm_day = dia_;
+	f->tm_mon = mes_ -1;
+	f->tm_year = anno_ - 1900;
+	mktime(f);
+	strftime(aux, 100, "%A %e de %B de %Y", f);
+	return aux;
+}
+
+Fecha& Fecha::operator +=(int dias)
+{
+	std::tm f{};
+	f.tm_mday = this.dia_+dias;
+	f.tm_mon = this.mes_ - 1;
+	f.tm_year = this.anno_ - 1900;
+	std::mktime(&f);
+
+	dia_ = f.tm_day;
+	mes_ = f.tm_mon + 1;
+	anno_ = f.tm_year + 1900;
+
+	esValida();
+	return *this;
+}
+
+Fecha& Fecha::operator -=(int dias)
+{
+	*this += -dias;
+	return *this;
+}
+
+Fecha& Fecha::operator ++()
+{
+	*this += 1;
+	return *this;
+}
+
+Fecha& Fecha::operator ++(int)
+{
+	Fecha *f = new Fecha(*this);
+
+	*this += 1;
+	return *f;
+}
+
+Fecha& Fecha::operator --()
+{
+	*this += -1;
+	return *this;
+}
+
+Fecha& Fecha::operator --(int)
+{
+	Fecha *f = new Fecha(*this);
+
+	*this += -1;
+	return *f;
+}
+
+Fecha Fecha::operator +(int dias) const
+{
+	Fecha f(*this);
+	return f += dias;
+}
+
+Fecha Fecha::operator -(int dias) const
+{
+	Fecha f(*this);
+	return f += -dias;
+}
+
+bool operator ==(const Fecha& f, const Fecha& g)
+{
+	return (f.dia_ == g.dia_ && f.mes_ == g.mes_ && f.anno_ == g.anno_);
+}
+
+bool operator !=(const Fecha& f, const Fecha& g)
+{
+	return !(f == g);
+}
+
+bool operator <(const Fecha& f, const Fecha& g)
+{
+	return (f.anno_ g.anno_ || (f.anno_ == g.anno_ && f.mes_ < g.mes_ || (f.mes_ == g.mes_ && f.dia_ < g.dia_)));
+}
+
+
+bool operator >(const Fecha& f, const Fecha& g)
+{
+	return g < f;
+}
+
+bool operator <=(const Fecha& f, const Fecha& g)
+{
+	return !(g < f);
+}
+
+bool operator >=(const Fecha& f, const Fecha& g)
+{
+	return !(f < g);
+}
--- Munnoz_Morales_Jonathan/P0/fecha.hpp
+++ Munnoz_Morales_Jonathan/P0/fecha.hpp
@@ -1,15 +1,59 @@
-#include <iostream>
+#ifndef _FECHA_HPP_
+#define _FECHA_HPP_
 
 public class Fecha{
 public:
+
 	//Contantes públicas
 	static const int AnnoMinimo = 1902;
 	static const int AnnoMaximo = 2037;
+
 	//clase pública Invalida
 	public class Invalida{
-		Invalida(const char* motivo){}
+	public:
+		Invalida(const char* porque){std::strcpy(porque_, porque);}
+		inline const char* por_que(){return porque_;}
+	private:
+		char* porque_;
 	};
+
 	//Contructores
 	Fecha(int dia = 0, int mes = 0, int anno = 0);
 	Fecha(const char* fecha);
+	Fecha(const Fecha& f) = default;
+
+	//Observadores
+	inline int dia() const noexcept{return dia_;}
+	inline int mes() const noexcept{return mes_;}
+	inline int anno() const noexcept{return anno_;}
+
+	//Operadores no modificadores
+	Fecha operator +(int dias) const;
+	Fecha operator -(int dias) const;
+
+	//Operadores modificadores
+	operator const char*() const;
+	Fecha& operator ++();
+	Fecha& operator ++(int);
+	Fecha& operator --();
+	Fecha& operator --(int);
+	Fecha& operator +=(int dias);
+	Fecha& operator -=(int dias);
+
+	//Metodos amigos(friend)
+	friend Fecha& operator <(const Fecha& f, const Fecha& g);
+	friend Fecha& operator <=(const Fecha& f, const Fecha& g);
+	friend Fecha& operator ==(const Fecha& f, const Fecha& g);
+	friend Fecha& operator !=(const Fecha& f, const Fecha& g);
+	friend Fecha& operator >(const Fecha& f, const Fecha& g);
+	friend Fecha& operator <=(const Fecha& f, const Fecha& g);
+
+	//Destructor
+	~Fecha() = default;
+private:
+	int dia_, mes_, anno_;
+	void esValida() const;
+	bool esBisiesto() const;
 };
+
+#endif//_FECHA_HPP_
