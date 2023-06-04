511aeb7ac99790c49a4972985d7c6903d3ff46c2
jonathanMM97 <jonathan_tuenty@hotmail.com>
2023-06-04 13:19:57 +0200

    Corrigiendo error en articulo.hpp

--- Munnoz_Morales_Jonathan/P2/articulo.hpp
+++ Munnoz_Morales_Jonathan/P2/articulo.hpp
@@ -19,23 +19,23 @@ private:
     const Cadena title;
     const Fecha release;
     double price;
-    int stock;
+    int copies;
 
 public:
     //Constructor...
     Articulo(Cadena reference, Cadena title, Fecha date, double price, int stock): 
-    reference(reference), title(title), release(date), price(price), stock(stock){};
+    reference(reference), title(title), release(date), price(price), copies(stock){};
 
     //Observables...
     Cadena referencia() const{return reference;};
     Cadena titulo() const{return title;};
     Fecha f_publi() const{return release;};
     double precio() const{return price;};
-    int stock() const{return stock;};
+    int stock() const{return copies;};
 
     //Modificadores...
     double& precio(){return price;};
-    int& stock(){return stock;};
+    int& stock(){return copies;};
         
 };
 
