8b4813cfbc93b5f639eee9dc3c1b322a5942fa5d
jonathanMM97 <jonathan_tuenty@hotmail.com>
2023-06-04 13:24:34 +0200

    Corregido error por lo que no se reconocia set

--- Munnoz_Morales_Jonathan/P2/tarjeta.hpp
+++ Munnoz_Morales_Jonathan/P2/tarjeta.hpp
@@ -16,99 +16,99 @@
 #include "fecha.hpp"
 #include "usuario.hpp"
 
+using namespace std;
+
+
 class Usuario;
 
 class Numero 
 {
-    private:
-        Cadena num_;
-    public:
-        Numero(const Cadena& num);//Constructor
-        //Operador de conversión a cadena de bajo nivel
-        operator const char*() const {return(num_.c_str());}
+private:
+    Cadena num_;
+public:
+    Numero(const Cadena& num);//Constructor
+    //Operador de conversión a cadena de bajo nivel
+    operator const char*() const {return(num_.c_str());}
 
-        enum Razon {LONGITUD, DIGITOS, NO_VALIDO};  //Tipo enumerado para Incorrecto
-        class Incorrecto    //Clase para excepciones
-        {
-            private:
-                Razon razon_;
-            public:
-                Incorrecto(Razon r): razon_{r} {} //Constructor
-                const Razon razon() const {return(razon_);} //Observador de la razón
-        };
+    enum Razon {LONGITUD, DIGITOS, NO_VALIDO};  //Tipo enumerado para Incorrecto
+    class Incorrecto    //Clase para excepciones
+    {
+        private:
+            Razon razon_;
+        public:
+            Incorrecto(Razon r): razon_{r} {} //Constructor
+            const Razon razon() const {return(razon_);} //Observador de la razón
+    };
 };
 
 bool operator<(const Numero& a, const Numero& b) noexcept;
 
 class Tarjeta 
 {
-    private:
-        const Numero numero_;
-        const Usuario* titular_;
-        const Fecha caducidad_;
-        bool activa_;
-        static set<Numero> conjunto;
+private:
+    const Numero numero_;
+    const Usuario* titular_;
+    const Fecha caducidad_;
+    bool activa_;
+    static set<Numero> conjunto;
 
-        //Para cuando se destruya el titular, solo tienen acceso los Usuario
-        void anula_titular();
+    //Para cuando se destruya el titular, solo tienen acceso los Usuario
+    void anula_titular();
 
-    public:
+public:
 
-        friend class Usuario; 
+    friend class Usuario; 
 
-        //Enumeración pública de tipos de Tarjeta
-        enum Tipo{Otro=0, VISA=1, Mastercard=2, Maestro=3, JCB=4, AmericanExpress=5};
+    //Enumeración pública de tipos de Tarjeta
+    enum Tipo{Otro=0, VISA=1, Mastercard=2, Maestro=3, JCB=4, AmericanExpress=5};
 
-        //Constructor
-        Tarjeta(const Numero& num, Usuario& user, const Fecha& caducidad);
-            /*  TODO:  
-                El constructor deberá asociar la tarjeta que se está creando con su Usuario correspondiente,
-                llamando a Usuario::es_titular_de sobre su titular.
-            */
-        //Clase para las excepciones de tarjeta caducada
-        class Caducada
-        {
-            private:
-                Fecha caducada_;
-            public:
-                Caducada(const Fecha& f): caducada_{f}{}
-                const Fecha cuando() const{return(caducada_);}
-        };
+    //Constructor
+    Tarjeta(const Numero& num, Usuario& user, const Fecha& caducidad);
+
+    //Clase para las excepciones de tarjeta caducada
+    class Caducada
+    {
+        private:
+            Fecha caducada_;
+        public:
+            Caducada(const Fecha& f): caducada_{f}{}
+            const Fecha cuando() const{return(caducada_);}
+    };
         
-        //Clase para las excepciones de numero de tarjeta repetido
-        class Num_duplicado
-        {
-            private:
-                Numero duplicado_;
-            public:
-                Num_duplicado(const Numero& dup): duplicado_{dup}{}
-                const Numero que() const{return(duplicado_);}
-        };
+    //Clase para las excepciones de numero de tarjeta repetido
+    class Num_duplicado
+    {
+        private:
+            Numero duplicado_;
+        public:
+            Num_duplicado(const Numero& dup): duplicado_{dup}{}
+            const Numero que() const{return(duplicado_);}
+    };
 
-        //Constructor de copia anulado
-        Tarjeta(const Tarjeta&) = delete;
+    //Constructor de copia anulado
+    Tarjeta(const Tarjeta&) = delete;
 
-        //Operador de asignación anulado
-        Tarjeta& operator=(const Tarjeta& cad) = delete;
+    //Operador de asignación anulado
+    Tarjeta& operator=(const Tarjeta& cad) = delete;
 
-        //Observadores
-        const Numero& numero() const noexcept{return(numero_);}
-        const Usuario* titular() const noexcept{return(titular_);}
-        const Fecha& caducidad() const noexcept{return(caducidad_);}
-        bool activa() const noexcept{return(activa_);}
-        Tipo tipo() const noexcept;
+    //Observadores
+    const Numero& numero() const noexcept{return(numero_);}
+    const Usuario* titular() const noexcept{return(titular_);}
+    const Fecha& caducidad() const noexcept{return(caducidad_);}
+    bool activa() const noexcept{return(activa_);}
+    Tipo tipo() const noexcept;
 
-        //Activar o deszactivar tarjeta
-        bool activa(bool a=true);
+    //Activar o deszactivar tarjeta
+    bool activa(bool a=true);
 
-        //Clase de excepción Desactivada
-        class Desactivada
-        {
-            //Vacía, para uso posterior en otros casos de uso.
-        };
+    //Clase de excepción Desactivada
+    class Desactivada
+    {
+        //Vacía, para uso posterior en otros casos de uso.
+    };
 
-        //Destructor
-        ~Tarjeta();
+    //Destructor
+    ~Tarjeta();
 };
 
 bool operator<(const Tarjeta& a, const Tarjeta& b) noexcept;
